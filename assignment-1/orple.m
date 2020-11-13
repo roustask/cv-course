b9d3311c217c59a34e010585ea4cf7b6baff8e73
Konstantinos Roustas <56471293+roustask@users.noreply.github.com>
2020-11-13 20:23:55 +0200

    Update orple.m

--- assignment-1/orple.m
+++ assignment-1/orple.m
@@ -14,14 +14,15 @@ maskb = 1-maska;
 blurh = fspecial('gauss',30,15); % feather the border
 maska = imfilter(maska,blurh,'replicate');
 maskb = imfilter(maskb,blurh,'replicate');
+Gmaska = genPyr(maska, 'gauss', level); %mask gaussian pyramids
+Gmaskb = genPyr(maskb, 'gauss', level);
+
 limgo = cell(1,level); % the blended pyramid
 for p = 1:level
 	[Mp Np ~] = size(limga{p});
-	maskap = imresize(maska,[Mp Np]);
-	maskbp = imresize(maskb,[Mp Np]);
-	limgo{p} = limga{p}.*maskap + limgb{p}.*maskbp;
+	Gmaska{p} = imresize(Gmaska{p},[Mp Np]);
+	Gmaskb{p} = imresize(Gmaskb{p},[Mp Np]);
+	limgo{p} = limga{p}.*Gmaska{p} + limgb{p}.*Gmaskb{p};
 end
 imgo = pyrReconstruct(limgo);
 figure,imshow(imgo) % blend by pyramid
-imgo1 = maska.*imga+maskb.*imgb;
-figure,imshow(imgo1) % blend by feathering
